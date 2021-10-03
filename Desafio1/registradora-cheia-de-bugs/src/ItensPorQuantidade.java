cdc0f976f5ec0922e999a9949b6c6b60efe71de1
GabSouza98 <gabrielaraujodesouza98@gmail.com>
2021-10-03 00:30:19 -0300

    fix - arruma inconsistencia no método retiraItem para o caso de paes
    Transforma a quantidade de pães passada para gramas de pão, pois o estoque
    avalia necessidade de reposição com base em gramas e não em unidades.
    
    Método reporItem ajustado para repor 64 fatias de torta, pois o estoque
    avalia necessidade de reposição com base em fatias de torta.

--- Desafio1/registradora-cheia-de-bugs/src/ItensPorQuantidade.java
+++ Desafio1/registradora-cheia-de-bugs/src/ItensPorQuantidade.java
@@ -11,22 +11,27 @@ public class ItensPorQuantidade {
 
         if ("pao".equals(item)) {
             quantidadeEmEstoque = pao;
+            System.out.println(String.format("Temos %d gramas de pao." + pao ));
         }
 
         if ("torta".equals(item)) {
             quantidadeEmEstoque = torta;
+            System.out.println(String.format("Temos %d fatias de torta." + torta ));
         }
 
         if ("sanduiche".equals(item)) {
             quantidadeEmEstoque = sanduiche;
+            System.out.println(String.format("Temos %d sanduiches prontos." + sanduiche ));
         }
 
         if ("leite".equals(item)) {
             quantidadeEmEstoque = leite;
+            System.out.println(String.format("Temos %d unidades de leite." + leite ));
         }
 
         if ("cafe".equals(item)) {
             quantidadeEmEstoque = cafe;
+            System.out.println(String.format("Temos %d unidades de cafe." + cafe ));
         }
 
         return quantidadeEmEstoque;
@@ -35,8 +40,8 @@ public class ItensPorQuantidade {
     public static void retiraItem(String item, int quantidade) {
 
         if ("pao".equals(item)) {
-            if (pao - quantidade >= 0) {
-                pao -= quantidade;
+            if (pao - quantidade*60 >= 0) {
+                pao -= quantidade*60;        //converte unidades em gramas de pao
             } else {
                 pao = 0;
             }
