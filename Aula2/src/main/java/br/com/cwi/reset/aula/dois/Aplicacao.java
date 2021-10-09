b3eb9a3c81e60d31ac54d30bed32f157f1694580
GabSouza98 <gabrielaraujodesouza98@gmail.com>
2021-10-09 20:22:14 -0300

    Inclui a parte Opcional

--- Aula2/src/main/java/br/com/cwi/reset/aula/dois/Aplicacao.java
+++ Aula2/src/main/java/br/com/cwi/reset/aula/dois/Aplicacao.java
@@ -2,6 +2,8 @@ package br.com.cwi.reset.aula.dois;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Aplicacao {
 
@@ -12,20 +14,25 @@ public class Aplicacao {
 
         Ator ator1 = new Ator("Fulaninho", LocalDate.parse("1990-01-30", DateTimeFormatter.ofPattern("yyyy-MM-dd")), Genero.NAO_BINARIO, 5);
 
+        List<Filme> filmes = new ArrayList<>();
 
         try {
             Filme filme1 = new Filme("Pulp Fiction","Filme doido",120,1995,5,diretor1);
             Filme filme2 = new Filme("ET","Filme sobre a chegada de um ET na Terra",120,1982,5,diretor2);
+
+            filmes.add(filme1);
+            filmes.add(filme2);
+
+            for(Filme f : filmes) {
+                f.reproduzirFilme();
+            }
+
         }
         catch (AvaliacaoForaDoPadraoException e) {
             System.out.println(e.getMessage());
         }
 
-
-        //filme1.reproduzirFilme();
-        //filme2.reproduzirFilme();
-
         //diretor2.imprimeCaracteristicas();
-        ator1.imprimeCaracteristicas();
+        //ator1.imprimeCaracteristicas();
     }
 }
